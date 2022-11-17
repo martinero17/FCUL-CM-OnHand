84c0cbb97f4d7ceaee8ba92954d9a38d29ca574e
FernandoMacacao <80410580+FernandoMacacao@users.noreply.github.com>
2022-11-17 16:22:23 +0000

    first commit

---
+++ app/src/androidTest/java/com/example/fcul_cm_onhand/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.example.fcul_cm_onhand
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.example.fcul_cm_onhand", appContext.packageName)
+    }
+}
---
+++ app/src/main/java/com/example/fcul_cm_onhand/screens/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.fcul_cm_onhand.screens.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fcul_cm_onhand.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
