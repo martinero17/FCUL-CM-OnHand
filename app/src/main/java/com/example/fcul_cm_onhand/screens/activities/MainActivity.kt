d5f976adac6daa3790250be5e15ea0699a938c08
FernandoMacacao <80410580+FernandoMacacao@users.noreply.github.com>
2022-11-17 21:22:18 +0000

    xml login page implemented

--- app/src/main/java/com/example/fcul_cm_onhand/screens/activities/MainActivity.kt
+++ app/src/main/java/com/example/fcul_cm_onhand/screens/activities/MainActivity.kt
@@ -7,6 +7,6 @@ import com.example.fcul_cm_onhand.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login_screen)
     }
 }
