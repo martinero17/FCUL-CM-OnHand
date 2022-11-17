fbc9856073aaaec5afecf0df4e071b4ac5e97c0a
FernandoMacacao <80410580+FernandoMacacao@users.noreply.github.com>
2022-11-17 23:08:25 +0000

    bottom bar implemented

--- app/src/main/java/com/example/fcul_cm_onhand/screens/activities/MainActivity.kt
+++ app/src/main/java/com/example/fcul_cm_onhand/screens/activities/MainActivity.kt
@@ -3,10 +3,14 @@ package com.example.fcul_cm_onhand.screens.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.fcul_cm_onhand.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.register_screen)
+        setContentView(R.layout.activity_home_care_receiver)
+
     }
+
 }
