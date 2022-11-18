32eac1d70e30f7336788a55b2388d8dfc1d1af99
FernandoMacacao <80410580+FernandoMacacao@users.noreply.github.com>
2022-11-18 00:20:04 +0000

    bottom bar added to layout

--- app/src/main/java/com/example/fcul_cm_onhand/screens/activities/MainActivity.kt
+++ app/src/main/java/com/example/fcul_cm_onhand/screens/activities/MainActivity.kt
@@ -1,16 +1,37 @@
 package com.example.fcul_cm_onhand.screens.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.fcul_cm_onhand.R
+import com.example.fcul_cm_onhand.screens.activities.care_receiver.HomeCareReceiverActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.navigation.NavigationBarView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_care_receiver)
 
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.home_button -> {
+                    // Respond to navigation home button click
+                    startActivity(Intent(applicationContext, HomeCareReceiverActivity::class.java))
+                    overridePendingTransition(0, 0)
+                    true
+                }
+                R.id.medicine_button -> {
+                    // Respond to navigation medicine button click
+                    true
+                }
+                R.id.settings_button -> {
+                    // Respond to navigation settings button click
+                    true
+                }
+                else -> false
+            }
+        }
     }
 
 }
