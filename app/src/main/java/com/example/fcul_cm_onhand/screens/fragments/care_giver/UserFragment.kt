304ec40d219ce54800210372a19f93a2b09be110
FernandoMacacao <80410580+FernandoMacacao@users.noreply.github.com>
2022-11-24 00:27:49 +0000

    user fragment added

---
+++ app/src/main/java/com/example/fcul_cm_onhand/screens/fragments/care_giver/UserFragment.kt
@@ -0,0 +1,8 @@
+package com.example.fcul_cm_onhand.screens.fragments.care_giver
+
+import androidx.fragment.app.Fragment
+import com.example.fcul_cm_onhand.R
+
+class UserFragment : Fragment(R.layout.fragment_user) {
+    
+}
