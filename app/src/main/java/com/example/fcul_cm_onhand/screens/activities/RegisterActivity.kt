cc78e9810a570f91a3f979a103a9306a3b39d3fe
FernandoMacacao <80410580+FernandoMacacao@users.noreply.github.com>
2022-12-27 11:34:31 +0000

    Register inputs verification

--- app/src/main/java/com/example/fcul_cm_onhand/screens/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/fcul_cm_onhand/screens/activities/RegisterActivity.kt
@@ -3,12 +3,17 @@ package com.example.fcul_cm_onhand.screens.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.view.isEmpty
 import com.example.fcul_cm_onhand.R
 import com.example.fcul_cm_onhand.model.UserType
 import com.example.fcul_cm_onhand.screens.activities.main.MainActivity
@@ -23,14 +28,64 @@ class RegisterActivity : AppCompatActivity() {
         }
 
         findViewById<Button>(R.id.register_button).setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            intent.putExtra("UserType", UserType.CARE_GIVER)
-            startActivity(intent)
+            if(verifyCredentials()){
+                val intent = Intent(this, MainActivity::class.java)
+                intent.putExtra("UserType", UserType.CARE_GIVER) // TODO: REMOVE LATER
+                startActivity(intent)
+                finish()
+            }
 
-            finish()
         }
     }
 
+    private fun verifyCredentials(): Boolean {
+        val name = findViewById<EditText>(R.id.name_input).text
+        val username = findViewById<EditText>(R.id.username_input).text
+        val password = findViewById<EditText>(R.id.password_input).text
+        val confirmPassword = findViewById<EditText>(R.id.re_password_input).text
+        val type = findViewById<RadioGroup>(R.id.radio_types)
+
+        if(inputIsEmpty(name, username, password, confirmPassword, type) ||
+            !passwordMatch(password, confirmPassword)){
+            return false
+        }
+        return true
+    }
+
+    private fun passwordMatch(password: Editable, confirmPassword: Editable): Boolean{
+        if(password != confirmPassword){
+            toastMessage("Password doesn't match!")
+            return false
+        }
+        return true
+    }
+
+    private fun inputIsEmpty(name: Editable, username: Editable, password: Editable,
+                             confirmPassword: Editable, type: RadioGroup): Boolean{
+        if(name.isEmpty()){
+            toastMessage("Please fill name!")
+            return true
+        } else if(username.isEmpty()){
+            toastMessage("Please fill username!")
+            return true
+        } else if(password.isEmpty()){
+            toastMessage("Please fill password!")
+            return true
+        } else if(confirmPassword.isEmpty()){
+            toastMessage("Please confirm password!")
+            return true
+        } else if(type.isEmpty()){
+            toastMessage("Please select one type!")
+            return true
+        }
+        return false
+    }
+
+    private fun toastMessage(text: String){
+        Toast.makeText(applicationContext, text, Toast.LENGTH_SHORT).show()
+    }
+
+    // Response to radio button click event for user type
     fun onRadioButtonClicked(view: View) {
         if (view is RadioButton) {
             // Is the button now checked?
